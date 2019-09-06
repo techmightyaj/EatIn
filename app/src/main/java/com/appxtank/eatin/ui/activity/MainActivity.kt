b588e4ef2cd431a25ce98a68048af1470d7bd08d
Ankit Varia <variaankit@hotmail.com>
2019-09-06 18:03:07 +0530

    - Modularize code.

--- app/src/main/java/com/appxtank/eatin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/appxtank/eatin/ui/activity/MainActivity.kt
@@ -3,24 +3,16 @@ package com.appxtank.eatin.ui.activity
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Switch
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.appxtank.eatin.MyApplication
 import com.appxtank.eatin.R
-import com.appxtank.eatin.data.remote.response.ExcludeList
 import com.appxtank.eatin.data.remote.response.MenuResponse
-import com.appxtank.eatin.data.remote.response.Variation
 import com.appxtank.eatin.di.ActivityModule
 import com.appxtank.eatin.di.component.DaggerActivityComponent
 import com.appxtank.eatin.ui.adapter.MenuItemAdapter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -28,11 +20,6 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = MainActivity::class.java.simpleName
-    private lateinit var vegSwitch: Switch
-    private lateinit var menuItemTitle: TextView
-    private lateinit var menuItemChangeButton: Button
-    private lateinit var menuItemProceedButton: Button
-    private lateinit var menuItemsRecyclerView: RecyclerView
     private lateinit var menuItemAdapter: MenuItemAdapter
 
     @Inject
@@ -43,25 +30,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        init()
-        setUpRecyclerView(vegSwitch.isChecked)
+        setUpRecyclerView(switch_veg.isChecked)
         observeData()
         fetchData()
         switchClick()
         proceedClick()
     }
 
-    private fun init() {
-        vegSwitch = findViewById(R.id.switch_veg)
-        menuItemTitle = findViewById(R.id.tv_menu_item_header)
-        menuItemsRecyclerView = findViewById(R.id.rv_menu_item)
-        menuItemProceedButton = findViewById(R.id.btn_proceed)
-        menuItemChangeButton = findViewById(R.id.btn_change)
-    }
-
     private fun setUpRecyclerView(isOnlyVeg: Boolean) {
         menuItemAdapter = MenuItemAdapter(this, isOnlyVeg)
-        menuItemsRecyclerView.apply {
+        rv_menu_item.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = menuItemAdapter
             setHasFixedSize(true)
@@ -79,7 +57,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun switchClick() {
-        vegSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+        switch_veg.setOnCheckedChangeListener { buttonView, isChecked ->
             setUpRecyclerView(isOnlyVeg = isChecked)
             menuItemAdapter.setMenuItems(viewModel.menuResponse.value?.variants?.variant_groups!![viewModel.selectedVariants].variations)
             menuItemAdapter.notifyDataSetChanged()
@@ -88,27 +66,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeClick() {
         if (viewModel.selectedVariants > 0) {
-            menuItemChangeButton.visibility = View.VISIBLE
-            menuItemChangeButton.setOnClickListener { v ->
+            btn_change.visibility = View.VISIBLE
+            btn_change.setOnClickListener { v ->
                 viewModel.selectedVariants = 0
                 viewModel.selectedVariation = null
                 fetchData()
-                setUpRecyclerView(vegSwitch.isChecked)
+                setUpRecyclerView(switch_veg.isChecked)
                 //setAdapterData(null)
                 changeClick()
             }
         } else {
-            menuItemChangeButton.visibility = View.GONE
+            btn_change.visibility = View.GONE
         }
     }
 
     private fun proceedClick() {
-        menuItemProceedButton.setOnClickListener { v ->
+        btn_proceed.setOnClickListener { v ->
             if (menuItemAdapter.getSelectedVariation() != null) {
                 viewModel.selectedVariants++
                 viewModel.selectedVariation = menuItemAdapter.getSelectedVariation()
                 changeClick()
-                setUpRecyclerView(vegSwitch.isChecked)
+                setUpRecyclerView(switch_veg.isChecked)
                 setAdapterData(null)
             } else {
                 Toast.makeText(this, "Select any item.", Toast.LENGTH_SHORT).show()
@@ -121,9 +99,9 @@ class MainActivity : AppCompatActivity() {
         val menuResponse: MenuResponse = mR ?: viewModel.menuResponse.value!!
         menuResponse.let {
             if(viewModel.selectedVariants < menuResponse.variants.variant_groups.size) {
-                menuItemTitle.text =
+                tv_menu_item_header.text =
                     menuResponse.variants.variant_groups[viewModel.selectedVariants].name
-                menuItemAdapter.setMenuItems(checkForExcludedItem(menuResponse.variants.variant_groups[viewModel.selectedVariants].variations))
+                menuItemAdapter.setMenuItems(viewModel.checkForExcludedItem(menuResponse.variants.variant_groups[viewModel.selectedVariants].variations))
             }else{
                 btn_change.visibility = View.GONE
                 switch_veg.visibility = View.GONE
@@ -131,46 +109,6 @@ class MainActivity : AppCompatActivity() {
                 btn_proceed.visibility = View.GONE
             }
         }
-    }
-
-    private fun checkForExcludedItem(variationList: List<Variation>): List<Variation> {
-        if (viewModel.selectedVariation == null) {
-            return variationList
-        }
-
-
-        val menuResponse: MenuResponse = viewModel.menuResponse.value!!
-        val listType = object : TypeToken<ArrayList<ExcludeList>>() {}.type
-        var excludeList: ArrayList<ArrayList<ExcludeList>>? = ArrayList()
-
-        for (elementList in menuResponse.variants.exclude_list)
-            excludeList?.add(
-                Gson().fromJson<ArrayList<ExcludeList>>(
-                    elementList.toString(),
-                    listType
-                )
-            )
-
-        if (excludeList != null && excludeList.size > 0) {
-            for (excludeItemList in excludeList) {
-                for (i in 0 until excludeItemList.size) {
-                    if (excludeItemList[i].group_id.toInt() == viewModel.selectedVariants
-                        && excludeItemList[i].variation_id.toInt() == viewModel.selectedVariation!!.id.toInt()) {
-                        if(i != 1) {
-                            for (variationItem in variationList) {
-                                if (excludeItemList[i + 1].variation_id.toInt() == variationItem.id.toInt())
-                                    variationItem.isExcluded = true
-                            }
-                        }else{
-                            continue
-                        }
-                    }
-                }
-            }
-        }
-
-
-        return variationList
     }
 
     private fun getDependencies() {
--- app/src/main/java/com/appxtank/eatin/ui/activity/MainViewModel.kt
+++ app/src/main/java/com/appxtank/eatin/ui/activity/MainViewModel.kt
@@ -4,9 +4,12 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.appxtank.eatin.data.local.DatabaseService
 import com.appxtank.eatin.data.remote.NetworkService
+import com.appxtank.eatin.data.remote.response.ExcludeList
 import com.appxtank.eatin.data.remote.response.MenuResponse
 import com.appxtank.eatin.data.remote.response.Variation
 import com.appxtank.eatin.di.ActivityScope
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
@@ -41,11 +44,44 @@ class MainViewModel @Inject constructor(
         )
     }
 
-//    fun parseMenuItems(menuResponse: MenuResponse){
-//        var map = LinkedHashMap<VariantGroup, List<Variation>>()
-//        for(i in menuResponse.variants.variant_groups.indices)
-//            map[menuResponse.variants.variant_groups[i]] = menuResponse.variants.variant_groups[i].variations
-//
-//        parsedMenuItems.postValue(map)
-//    }
+    fun checkForExcludedItem(variationList: List<Variation>): List<Variation> {
+        if (selectedVariation == null) {
+            return variationList
+        }
+
+
+        val menuResponse: MenuResponse = menuResponse.value!!
+        val listType = object : TypeToken<ArrayList<ExcludeList>>() {}.type
+        var excludeList: ArrayList<ArrayList<ExcludeList>>? = ArrayList()
+
+        for (elementList in menuResponse.variants.exclude_list)
+            excludeList?.add(
+                Gson().fromJson<ArrayList<ExcludeList>>(
+                    elementList.toString(),
+                    listType
+                )
+            )
+
+        if (excludeList != null && excludeList.size > 0) {
+            for (excludeItemList in excludeList) {
+                for (i in 0 until excludeItemList.size) {
+                    if (excludeItemList[i].group_id.toInt() == selectedVariants
+                        && excludeItemList[i].variation_id.toInt() == selectedVariation!!.id.toInt()
+                    ) {
+                        if (i != 1) {
+                            for (variationItem in variationList) {
+                                if (excludeItemList[i + 1].variation_id.toInt() == variationItem.id.toInt())
+                                    variationItem.isExcluded = true
+                            }
+                        } else {
+                            continue
+                        }
+                    }
+                }
+            }
+        }
+
+
+        return variationList
+    }
 }
